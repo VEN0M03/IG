feb04d510fdd5aac8c62976253a05d205a73c8fa
VEN0M03 <pythonvenom291@gmail.com>
2024-10-27 11:46:17 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ except:pass
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("ig58k")._____niki_()
+    __import__("ig58k")
 except Exception as e:
     exit(str(e))
