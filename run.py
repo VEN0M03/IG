6c4cc767af30526d42dc538c654b57c6f567afd3
VEN0M03 <pythonvenom291@gmail.com>
2024-10-27 11:43:27 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+import os, sys
+try:os.system('git pull')
+except:pass
+try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
+except:pass
+try:
+    __import__("ig58k")._____niki_()
+except Exception as e:
+    exit(str(e))
