6e592a780ee957a78543efe6c020e9de87e4d19a
VEN0M03 <pythonvenom291@gmail.com>
2024-10-27 11:41:31 +0100

    Delete rue.py

--- rue.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-try:os.system('git pull')
-except:pass
-try:os.system('xdg-open https://www.facebook.com/copnrt.03')
-except:pass
-try:
-    __import__("ig58k")._____niki_()
-except Exception as e:
-    exit(str(e))
